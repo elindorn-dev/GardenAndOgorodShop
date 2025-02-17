93d6013eedcbbbe702d0727c787bffc88329ebba
elindorn <development.code.elindorn@gmail.com>
2025-02-17 23:54:41 +0300

    добавлен дизайн формы аутентификации с комментариями (требуется доработка по объеденению функций)

---
+++ GardenAndOgorodShop/AuthForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace GardenAndOgorodShop
+{
+    partial class AuthForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AuthForm));
+            this.panelAuth = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAuth = new System.Windows.Forms.Button();
+            this.buttonMenu = new System.Windows.Forms.PictureBox();
+            this.panelSettings = new System.Windows.Forms.Panel();
+            this.buttonExitApp = new System.Windows.Forms.Button();
+            this.panelAuth.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonMenu)).BeginInit();
+            this.panelSettings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelAuth
+            // 
+            this.panelAuth.BackColor = System.Drawing.Color.White;
+            this.panelAuth.Controls.Add(this.panel3);
+            this.panelAuth.Controls.Add(this.panel2);
+            this.panelAuth.Controls.Add(this.label1);
+            this.panelAuth.Controls.Add(this.buttonAuth);
+            this.panelAuth.Location = new System.Drawing.Point(90, 34);
+            this.panelAuth.Name = "panelAuth";
+            this.panelAuth.Size = new System.Drawing.Size(400, 300);
+            this.panelAuth.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel3.Controls.Add(this.textBoxLogin);
+            this.panel3.Location = new System.Drawing.Point(23, 74);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(350, 50);
+            this.panel3.TabIndex = 4;
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBoxLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLogin.ForeColor = System.Drawing.Color.Gray;
+            this.textBoxLogin.Location = new System.Drawing.Point(17, 15);
+            this.textBoxLogin.MaxLength = 100;
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(319, 22);
+            this.textBoxLogin.TabIndex = 0;
+            this.textBoxLogin.Text = "Логин";
+            this.textBoxLogin.Enter += new System.EventHandler(this.textBoxLogin_Enter);
+            this.textBoxLogin.Leave += new System.EventHandler(this.textBoxLogin_Leave);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel2.Controls.Add(this.textBoxPassword);
+            this.panel2.Location = new System.Drawing.Point(23, 139);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(350, 50);
+            this.panel2.TabIndex = 3;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPassword.ForeColor = System.Drawing.Color.Gray;
+            this.textBoxPassword.Location = new System.Drawing.Point(17, 15);
+            this.textBoxPassword.MaxLength = 100;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(319, 22);
+            this.textBoxPassword.TabIndex = 0;
+            this.textBoxPassword.Text = "Пароль";
+            this.textBoxPassword.Enter += new System.EventHandler(this.textBoxPassword_Enter);
+            this.textBoxPassword.Leave += new System.EventHandler(this.textBoxPassword_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(175)))), ((int)(((byte)(80)))));
+            this.label1.Location = new System.Drawing.Point(16, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Вход в систему";
+            // 
+            // buttonAuth
+            // 
+            this.buttonAuth.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.buttonAuth.FlatAppearance.BorderSize = 0;
+            this.buttonAuth.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAuth.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAuth.ForeColor = System.Drawing.Color.White;
+            this.buttonAuth.Location = new System.Drawing.Point(23, 226);
+            this.buttonAuth.Name = "buttonAuth";
+            this.buttonAuth.Size = new System.Drawing.Size(350, 50);
+            this.buttonAuth.TabIndex = 1;
+            this.buttonAuth.Text = "Авторизоваться";
+            this.buttonAuth.UseVisualStyleBackColor = false;
+            // 
+            // buttonMenu
+            // 
+            this.buttonMenu.BackgroundImage = global::GardenAndOgorodShop.Properties.Resources.burger_menu;
+            this.buttonMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonMenu.Location = new System.Drawing.Point(534, 2);
+            this.buttonMenu.Name = "buttonMenu";
+            this.buttonMenu.Size = new System.Drawing.Size(50, 50);
+            this.buttonMenu.TabIndex = 1;
+            this.buttonMenu.TabStop = false;
+            this.buttonMenu.Click += new System.EventHandler(this.buttonMenu_Click);
+            // 
+            // panelSettings
+            // 
+            this.panelSettings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.panelSettings.Controls.Add(this.buttonExitApp);
+            this.panelSettings.Location = new System.Drawing.Point(587, 61);
+            this.panelSettings.Name = "panelSettings";
+            this.panelSettings.Size = new System.Drawing.Size(200, 75);
+            this.panelSettings.TabIndex = 2;
+            // 
+            // buttonExitApp
+            // 
+            this.buttonExitApp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.buttonExitApp.FlatAppearance.BorderSize = 0;
+            this.buttonExitApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExitApp.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExitApp.ForeColor = System.Drawing.Color.White;
+            this.buttonExitApp.Location = new System.Drawing.Point(17, 13);
+            this.buttonExitApp.Name = "buttonExitApp";
+            this.buttonExitApp.Size = new System.Drawing.Size(167, 50);
+            this.buttonExitApp.TabIndex = 5;
+            this.buttonExitApp.Text = "Выйти";
+            this.buttonExitApp.UseVisualStyleBackColor = false;
+            // 
+            // AuthForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(186)))), ((int)(((byte)(93)))));
+            this.ClientSize = new System.Drawing.Size(587, 362);
+            this.Controls.Add(this.panelSettings);
+            this.Controls.Add(this.buttonMenu);
+            this.Controls.Add(this.panelAuth);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AuthForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.AuthForm_Load);
+            this.panelAuth.ResumeLayout(false);
+            this.panelAuth.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonMenu)).EndInit();
+            this.panelSettings.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelAuth;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonAuth;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox buttonMenu;
+        private System.Windows.Forms.Panel panelSettings;
+        private System.Windows.Forms.Button buttonExitApp;
+    }
+}
+
---
+++ GardenAndOgorodShop/AuthForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GardenAndOgorodShop
+{
+    public partial class AuthForm : Form
+    {
+        // переменные для сохранения текста textbox при обработке placeholder
+        private string save_login_textbox;
+        private string save_password_textbox;
+        // переменная для обработки перемещения панели настроек
+        bool moving;
+        // переменная-флаг активации по нажатию кнопки
+        bool setting_active_status = false;
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+        // ФУНКЦИЯ ПОЯВЛЕНИЯ ПАНЕЛИ НАСТРОЕК
+        private async void ActiveSetting()
+        {
+            // задаём будущее положение панели настроек
+            int future_location_pannelSettings = panelSettings.Location.X - 210;
+            // цикл по пиксельного перемещения с итерационным созданием точки локации на форме
+            while (!moving && panelSettings.Location.X > future_location_pannelSettings)
+            {
+                moving = true;
+                await Task.Delay(1);
+                panelSettings.Location = new Point(panelSettings.Location.X - panelSettings.Location.X / 100, panelSettings.Location.Y);
+                moving = false;
+            }
+            setting_active_status = true;
+        }
+        // ФУНКЦИЯ СКРЫТИЯ ПАНЕЛИ НАСТРОЕК
+        private async void DisactiveSetting()
+        {
+            // задаём будущее положение панели настроек
+            int future_location_pannelSettings = panelSettings.Location.X + 210;
+            // цикл по пиксельного перемещения с итерационным созданием точки локации на форме
+            while (!moving && panelSettings.Location.X < future_location_pannelSettings)
+            {
+                moving = true;
+                await Task.Delay(1);
+                panelSettings.Location = new Point(panelSettings.Location.X + panelSettings.Location.X / 100, panelSettings.Location.Y);
+                moving = false;
+            }
+            setting_active_status = false;
+        }
+
+        private void AuthForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        // ФУНКЦИЯ ОБРАБОТКИ placeholder для логина при фокусе textbox
+        private void textBoxLogin_Enter(object sender, EventArgs e)
+        {
+            textBoxLogin.ForeColor = Color.Black;
+            if (save_login_textbox != "")
+            {
+                textBoxLogin.Text = save_login_textbox;
+            }
+            else
+            {
+                textBoxLogin.Text = "";
+            }
+        }
+        // ФУНКЦИЯ ОБРАБОТКИ placeholder для логина при уходе из фокуса textbox
+        private void textBoxLogin_Leave(object sender, EventArgs e)
+        {
+            if (textBoxLogin.Text == "")
+            {
+                textBoxLogin.Text = "Логин";
+                textBoxLogin.ForeColor = Color.Gray;
+                save_login_textbox = "";
+            }
+            else
+            {
+                save_login_textbox = textBoxLogin.Text;
+            }
+        }
+        // ФУНКЦИЯ ОБРАБОТКИ placeholder для пароля при фокусе textbox
+        private void textBoxPassword_Enter(object sender, EventArgs e)
+        {
+            textBoxPassword.ForeColor = Color.Black;
+            if (save_password_textbox != "")
+            {
+                textBoxPassword.Text = save_password_textbox;
+            }
+            else
+            {
+                textBoxPassword.Text = "";
+            }
+        }
+        // ФУНКЦИЯ ОБРАБОТКИ placeholder для пароля при уходе из фокуса textbox
+        private void textBoxPassword_Leave(object sender, EventArgs e)
+        {
+            
+            if (textBoxPassword.Text == "")
+            {
+                textBoxPassword.Text = "Пароль";
+                textBoxPassword.ForeColor = Color.Gray;
+                save_password_textbox = "";
+            }
+            else
+            {
+                save_password_textbox = textBoxPassword.Text;
+            }
+        }
+        // ФУНКЦИЯ АКТИВАЦИИ ПАНЕЛИ НАСТРОЕК
+        private void buttonMenu_Click(object sender, EventArgs e)
+        {
+            if (!setting_active_status)
+            {
+                ActiveSetting();
+            }
+            else
+            {
+                DisactiveSetting();
+            }
+        }
+    }
+}
