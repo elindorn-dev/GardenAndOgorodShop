04abdb38005db22624385f9ff4afa697bd7ed365
SmirnovD <d4449978@gmail.com>
2025-03-31 14:12:03 +0300

    Close #12. Created design of settings form

---
+++ GardenAndOgorodShop/dbSettingsForm.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace GardenAndOgorodShop
+{
+    partial class dbSettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.userpasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dbTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serverTextBox = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonRecovery = new System.Windows.Forms.Button();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.comboBoxTables = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Настройки";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.panel1.CausesValidation = false;
+            this.panel1.Location = new System.Drawing.Point(303, -12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(3, 414);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.panel2.Location = new System.Drawing.Point(125, 22);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(180, 3);
+            this.panel2.TabIndex = 12;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.buttonConnect.FlatAppearance.BorderSize = 2;
+            this.buttonConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonConnect.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonConnect.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.buttonConnect.Location = new System.Drawing.Point(20, 317);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(275, 50);
+            this.buttonConnect.TabIndex = 24;
+            this.buttonConnect.Text = "Установить соединение";
+            this.buttonConnect.UseVisualStyleBackColor = false;
+            this.buttonConnect.Click += new System.EventHandler(this.guest_button_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.label6.Location = new System.Drawing.Point(16, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(179, 21);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Пароль пользователя:";
+            // 
+            // userpasswordTextBox
+            // 
+            this.userpasswordTextBox.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userpasswordTextBox.Location = new System.Drawing.Point(20, 257);
+            this.userpasswordTextBox.MaxLength = 100;
+            this.userpasswordTextBox.Name = "userpasswordTextBox";
+            this.userpasswordTextBox.PasswordChar = '●';
+            this.userpasswordTextBox.Size = new System.Drawing.Size(279, 33);
+            this.userpasswordTextBox.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.label5.Location = new System.Drawing.Point(16, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 21);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Имя пользователя:";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.usernameTextBox.Location = new System.Drawing.Point(20, 195);
+            this.usernameTextBox.MaxLength = 50;
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(279, 33);
+            this.usernameTextBox.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.label4.Location = new System.Drawing.Point(12, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 21);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Название базы данных:";
+            // 
+            // dbTextBox
+            // 
+            this.dbTextBox.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dbTextBox.Location = new System.Drawing.Point(16, 131);
+            this.dbTextBox.MaxLength = 100;
+            this.dbTextBox.Name = "dbTextBox";
+            this.dbTextBox.Size = new System.Drawing.Size(279, 33);
+            this.dbTextBox.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 21);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Название сервера:";
+            // 
+            // serverTextBox
+            // 
+            this.serverTextBox.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.serverTextBox.Location = new System.Drawing.Point(16, 68);
+            this.serverTextBox.MaxLength = 100;
+            this.serverTextBox.Name = "serverTextBox";
+            this.serverTextBox.Size = new System.Drawing.Size(279, 33);
+            this.serverTextBox.TabIndex = 16;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.panel3.Location = new System.Drawing.Point(469, 22);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(135, 3);
+            this.panel3.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.label3.Location = new System.Drawing.Point(306, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 25);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Восстановление";
+            // 
+            // buttonRecovery
+            // 
+            this.buttonRecovery.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.buttonRecovery.FlatAppearance.BorderSize = 2;
+            this.buttonRecovery.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRecovery.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRecovery.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.buttonRecovery.Location = new System.Drawing.Point(315, 68);
+            this.buttonRecovery.Name = "buttonRecovery";
+            this.buttonRecovery.Size = new System.Drawing.Size(279, 50);
+            this.buttonRecovery.TabIndex = 27;
+            this.buttonRecovery.Text = "Восстановить структуру";
+            this.buttonRecovery.UseVisualStyleBackColor = false;
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.buttonImport.FlatAppearance.BorderSize = 2;
+            this.buttonImport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonImport.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonImport.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.buttonImport.Location = new System.Drawing.Point(327, 317);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(256, 50);
+            this.buttonImport.TabIndex = 28;
+            this.buttonImport.Text = "Импорт данных";
+            this.buttonImport.UseVisualStyleBackColor = false;
+            // 
+            // comboBoxTables
+            // 
+            this.comboBoxTables.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.comboBoxTables.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTables.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxTables.FormattingEnabled = true;
+            this.comboBoxTables.Location = new System.Drawing.Point(327, 258);
+            this.comboBoxTables.Name = "comboBoxTables";
+            this.comboBoxTables.Size = new System.Drawing.Size(256, 32);
+            this.comboBoxTables.TabIndex = 29;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.label7.Location = new System.Drawing.Point(323, 231);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 21);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Таблица:";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.panel4.Location = new System.Drawing.Point(389, 180);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(215, 3);
+            this.panel4.TabIndex = 32;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(120)))), ((int)(((byte)(56)))));
+            this.label8.Location = new System.Drawing.Point(306, 167);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 25);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "Импорт";
+            // 
+            // dbSettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(606, 400);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBoxTables);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.buttonRecovery);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.userpasswordTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dbTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.serverTextBox);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "dbSettingsForm";
+            this.Text = "dbSettingsForm";
+            this.Load += new System.EventHandler(this.dbSettingsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox userpasswordTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox dbTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox serverTextBox;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonRecovery;
+        private System.Windows.Forms.Button buttonImport;
+        private System.Windows.Forms.ComboBox comboBoxTables;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ GardenAndOgorodShop/dbSettingsForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Reflection;
+
+namespace GardenAndOgorodShop
+{
+    public partial class dbSettingsForm : Form
+    {
+        public dbSettingsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dbSettingsForm_Load(object sender, EventArgs e)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location);
+
+            config.AppSettings.Settings["host"].Value = host;
+            config.AppSettings.Settings["db"].Value = database;
+            config.AppSettings.Settings["uid"].Value = username;
+            config.AppSettings.Settings["pwd"].Value = pwd;
+
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private void guest_button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
